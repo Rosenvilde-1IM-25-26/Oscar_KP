a78f19fa1bf4d3389670d48959f989e9bfb0eec0
oselvg <80272228+oselvg@users.noreply.github.com>
2026-01-12 15:35:24 +0100

    js

--- MorsommeFiler/nyside/moro.js
+++ MorsommeFiler/nyside/moro.js
@@ -10,7 +10,7 @@ function sjekkTall(){
 
     let number = document.getElementById("brukerinput").value;
 
-    //Vi har valgt tallet 200
+    //Vi har valgt tallet 100
 
     if(number < 200){
         document.getElementById("resultat").innerHTML = "Liiiiiiite tall";
@@ -18,4 +18,17 @@ function sjekkTall(){
         document.getElementById("resultat").innerHTML = "Stooooooort tall";
     }
 
+}
+
+function billettPris(){
+
+    let alder = document.getElementById("brukerinput").value;
+
+    if(alder <= 13){
+        document.getElementById("resultat").innerHTML = "Barnebillett: 50kr";
+    }else if(alder > 13 && alder < 18){
+        document.getElementById("resultat").innerHTML = "Ungdomsbillett: 100kr";
+    }else{
+        document.getElementById("resultat").innerHTML = "Voksenbillett: 150kr";
+    }
 }
